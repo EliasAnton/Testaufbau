1d4dfff850163d7fc30056204342c596b5287580
Elias.Anton <elias.anton@relaxdays.de>
2023-04-24 11:53:51 +0200

    wip

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Net.Client;
+using GrpcService;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7214");
+var client = new Greeter.GreeterClient(channel);
+
+Console.WriteLine("Wie heist du?");
+var message = Console.ReadLine();
+
+var reply = await client.SayHelloAsync(new HelloRequest
+{
+    Name = message
+});
+
+Console.WriteLine("Greeting: " + reply.Message);
+
+Console.WriteLine("Shutting down");
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
---
+++ GrpcService/Program.cs
@@ -0,0 +1,23 @@
+﻿using GrpcService.Services;
+using MySqlConnector;
+using Testaufbau.DataAccess;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Database Context settings
+builder.Services.AddTransient<MySqlConnection>(_ =>
+    new MySqlConnection(builder.Configuration.GetConnectionString("MariaDb")));
+builder.Services.AddDbContext<MariaDbContext>(ServiceLifetime.Transient);
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGet("/",
+    ()
+        => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
