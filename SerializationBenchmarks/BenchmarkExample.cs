a2bc5f50bf44560b13f9f1b4cd92e63d455a4b25
Elias.Anton <elias.anton@relaxdays.de>
2023-04-27 14:54:21 +0200

    Json vs proto benchmarking

---
+++ SerializationBenchmarks/BenchmarkExample.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace SerializationBenchmarks;
+public class SingleVsFirst
+{
+    private readonly List<string> _haystack = new List<string>();
+    private readonly int _haystackSize = 1000000;
+    public List<string> _needles => new List<string> { "StartNeedle", "MiddleNeedle", "EndNeedle" };
+
+    public SingleVsFirst()
+    {
+        //Add a large amount of items to our list.
+        Enumerable.Range(1, _haystackSize).ToList().ForEach(x => _haystack.Add(x.ToString()));
+
+        //One at the start.
+        _haystack.Insert(0, _needles[0]);
+        //One right in the middle.
+        _haystack.Insert(_haystackSize / 2, _needles[1]);
+        //One at the end.
+        _haystack.Insert(_haystack.Count - 1, _needles[2]);
+    }
+
+    [ParamsSource(nameof(_needles))]
+    public string Needle { get; set; }
+
+    [Benchmark]
+    public string Single() => _haystack.SingleOrDefault(x => x == Needle);
+
+    [Benchmark]
+    public string First() => _haystack.FirstOrDefault(x => x == Needle);
+
+}
---
+++ SerializationBenchmarks/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Nodes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+using SerializationBenchmarks.Grpc;
+using SerializationBenchmarks.Json;
+
+
+//BenchmarkRunner.Run<ProtoSerialDeserialBenchmark>();
+BenchmarkRunner.Run<JsonSerialDeserialBenchmark>();
+
+
+
+//Examples:
+// var exampleClass = new JsonSerialDeserialExample();
+// exampleClass.SystemTextJsonSerialDeserialExample();
+// exampleClass.NewtonsoftJsonSerialDeserialExample();
